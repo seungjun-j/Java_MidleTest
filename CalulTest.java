dccb724c33a6be64567b8cd2f3d480abbff7a2cd
SeungJun <sj021210@naver.com>
2024-10-27 22:51:47 +0900

    ui 전체 수정

--- CalulTest.java
+++ CalulTest.java
@@ -2,57 +2,70 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CalulTest extends JFrame {
+
     CalulTest() {
         setTitle("계산기");
         setSize(520,250);
-
-        setLayout(new BorderLayout(10,10));
+        setLayout(new BorderLayout(1,1));
+        showNorth();
         showCenter();
+
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
-    void showCenter() {
-        JPanel p1 = new JPanel();
-        JPanel p2 = new JPanel();
-        JPanel panel = new JPanel(new GridLayout(2,0));
+    void showNorth(){
+        JPanel j1 = new JPanel(new GridLayout());
+        JPanel j2 = new JPanel();
+        JPanel j3 = new JPanel(new BorderLayout());
 
-        JTextField t1 = new JTextField(250);
-        t1.setEnabled(false);
-        p1.add(t1);
+        JComboBox cb1 = new JComboBox();
+        TextField tf1 = new TextField(5);
+        JButton his = new JButton("")
+        TextField tf = new TextField(100);
 
-        p2.setLayout(new GridLayout(5,5));
-        p2.add(new JButton("Backspace"));
-        p2.add(new JButton(""));
-        p2.add(new JButton(""));
-        p2.add(new JButton("CE"));
-        p2.add(new JButton("C"));
-        p2.add(new JButton("7"));
-        p2.add(new JButton("8"));
-        p2.add(new JButton("9"));
-        p2.add(new JButton("/"));
-        p2.add(new JButton("sqrt"));
-        p2.add(new JButton("4"));
-        p2.add(new JButton("5"));
-        p2.add(new JButton("6"));
-        p2.add(new JButton("x"));
-        p2.add(new JButton("%"));
-        p2.add(new JButton("1"));
-        p2.add(new JButton("2"));
-        p2.add(new JButton("3"));
-        p2.add(new JButton("-"));
-        p2.add(new JButton("1/x"));
-        p2.add(new JButton("0"));
-        p2.add(new JButton("+/-"));
-        p2.add(new JButton("."));
-        p2.add(new JButton("+"));
-        p2.add(new JButton("="));
-        panel.add(p1);
-        panel.add(p2);
-        add(panel,BorderLayout.CENTER);
+        cb1.setEditable(true);
+        j1.add(cb1,BorderLayout.WEST);
+        j1.add(tf1,BorderLayout.CENTER);
+        j2.add(tf, BorderLayout.CENTER);
+        j3.add(j1,BorderLayout.NORTH);
+        j3.add(j2,BorderLayout.SOUTH);
+
+        add(j3,BorderLayout.NORTH);
+
 
 
     }
+    void showCenter(){
+        JPanel j2 = new JPanel(new GridLayout(5,4));
+        JButton bk1 = new JButton("");
+        JButton bk2 = new JButton("");
+        JButton bk3 = new JButton("");
+        JButton bk4 = new JButton("");
+        JButton bk5 = new JButton("7");
+        JButton bk6 = new JButton("8");
+        JButton bk7 = new JButton("9");
+        JButton bk8 = new JButton("÷");
+        JButton bk9 = new JButton("4");
+        JButton bk10 = new JButton("5");
+        JButton bk11 = new JButton("6");
+        JButton bk12 = new JButton("x");
+        JButton bk13 = new JButton("1");
+        JButton bk14 = new JButton("2");
+        JButton bk15 = new JButton("3");
+        JButton bk16 = new JButton("-");
+        JButton bk17 = new JButton("/");
+        JButton bk18 = new JButton("0");
+        JButton bk19 = new JButton(".");
+        JButton bk20 = new JButton("=");
+        j2.add(bk1); j2.add(bk2);j2.add(bk3);j2.add(bk4);j2.add(bk5);j2.add(bk6);j2.add(bk7);j2.add(bk8);j2.add(bk9);j2.add(bk10);
+        j2.add(bk11);j2.add(bk12);j2.add(bk13);j2.add(bk14);j2.add(bk15);j2.add(bk16);j2.add(bk17);j2.add(bk18);j2.add(bk19);j2.add(bk20);
+
+        add(j2,BorderLayout.CENTER);
+
+    }
+
+
     public static void main(String[] args) {
         new CalulTest();
     }
