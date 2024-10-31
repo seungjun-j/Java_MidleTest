d29ff3e9ab86a16d53e89ff544d6be8bd11c154a
SeungJun <sj021210@naver.com>
2024-10-31 16:05:39 +0900

    추가 주석을 통한 코드 설명

--- CalulTest.java
+++ CalulTest.java
@@ -8,7 +8,7 @@ public class CalulTest extends JFrame {
     private JLabel result; // 결과 표시 레이블
     private JLabel info;   // 수식 입력 레이블
     private StringBuilder expression = new StringBuilder(); // 수식 저장
-
+//JFrame 기본 설정.
     CalulTest() {
         setTitle("계산기");
         setSize(520, 250);
@@ -18,13 +18,15 @@ public class CalulTest extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
-
+//JPanel1,Jpanel2를 분리하여 JFrame North부분에 들어갈 ui 제작
+//추후 추가적인 업데이트를 통해 다른 계산기를 추가할 수 있도록 콤보 박스를 사용하여 선택할 수 있게 만듬.
+//info, result Lable의 텍스트에 대한 글꼴 및 컬러를 지정하여 Panel에 추가함
     void showNorth() {
         JPanel j1 = new JPanel(new GridLayout(0, 3));
         JPanel j2 = new JPanel();
         JPanel j3 = new JPanel(new BorderLayout());
 
-        String[] kindCal = {"일반 계산기", "수면 시간 계산기"};
+        String[] kindCal = {"일반 계산기", ""};
         JComboBox<String> cb1 = new JComboBox<>(kindCal);
         result = new JLabel("0");
         result.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
@@ -45,7 +47,8 @@ public class CalulTest extends JFrame {
 
         add(j3, BorderLayout.NORTH);
     }
-
+//JFrame 계산기의 키패드를 Center에 배치, 그리고 ui의 가시성을 위해 버튼에 색상 추가
+// 이전 버전에서는 버튼을 하나씩 추가 하였지만 이번 버전에서는 배열을 사용하여 코드의 가독성을 좋게하기 위해 배열을 사용하여 업데이트 하였음.
     void showCenter() {
         JPanel j2 = new JPanel(new GridLayout(5, 4));
 
@@ -64,7 +67,7 @@ public class CalulTest extends JFrame {
 
         add(j2, BorderLayout.CENTER);
     }
-
+// ActionListener을 사용하여 계산기가 작동할 수 있도록 함. (Chat gpt를 참조.)
     private class ButtonClickListener implements ActionListener {
         private String label;
 
